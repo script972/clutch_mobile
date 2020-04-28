29e43616298b4cde854f58550b0a6cdce61c9cf5
DenisDevo <denys.misiura@extrawest.com>
2020-04-28 18:12:04 -0500

    add resources

---
+++ lib/ui/localization/keys.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'keys.dart';
+
+// **************************************************************************
+// Generator: FlutterTranslateGen
+// **************************************************************************
+
+class Keys {
+  static const String App_Name = 'app_name';
+
+  static const String Offers = 'offers';
+
+  static const String Companies = 'companies';
+
+  static const String About = 'about';
+
+  static const String Details = 'details';
+
+  static const String Collapse = 'collapse';
+
+  static const String Locations = 'locations';
+
+  static const String Profile = 'profile';
+}
